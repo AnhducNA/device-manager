f220a25556fedb458399750357497248e948aba5
padi-dev-ducla <ducla@paditech.com>
2024-08-22 16:35:31 +0700

    feat/register

--- src/services/user.service.js
+++ src/services/user.service.js
@@ -5,6 +5,14 @@ exports.getAllData = async () => {
 };
 
 exports.createData = async (dataParams) => {
-    const newData = User.create(dataParams)
-  return User.findAll();
+  const newData = User.create(dataParams);
+  return newData;
+};
+
+exports.getUserByEmail = async (email) => {
+  return await User.findOne({
+    attributes: { exclude: ["createdAt", "updatedAt"] },
+    where: { email: email },
+    raw: true,
+  });
 };
