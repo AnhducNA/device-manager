36c0b59328272aab2521f40945e455b8301285c3
padi-dev-ducla <ducla@paditech.com>
2024-08-25 23:47:51 +0700

    fix/get-data

--- src/controllers/device.controller.js
+++ src/controllers/device.controller.js
@@ -3,7 +3,7 @@ const deviceService = require("../services/device.service");
 exports.getAll = async (req, res) => {
   try {
     const data = await deviceService.getAllData();
-    res.status(200).json(data);
+    return res.status(200).json({ status: 1, data });
   } catch (error) {
     return res.status(500).json({ error });
   }
@@ -16,7 +16,7 @@ exports.getDetail = async (req, res) => {
     if (!data) {
       return res.status(400).json({ message: "do not have data" });
     }
-    return res.status(200).json(data);
+    return res.status(200).json({ status: 1, data });
   } catch (error) {
     return res.status(500).json({ error });
   }
@@ -26,7 +26,7 @@ exports.createData = async (req, res) => {
   try {
     const body = req.body;
     const data = await deviceService.createData(body);
-    return res.status(200).json(data);
+    return res.status(200).json({ status: 1, data });
   } catch (error) {
     return res.status(500).json({ error });
   }
