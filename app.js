0706e63962c8f73a936970063f5f718821f53118
padi-dev-ducla <ducla@paditech.com>
2024-08-25 23:10:17 +0700

    fix/cors 2

--- app.js
+++ app.js
@@ -40,7 +40,7 @@ app.use(express.urlencoded({ extended: true }));
 initRoute(app);
 
 // simple route
-app.get("/", cors(corsOptions), (req, res) => {
+app.get("/", (req, res) => {
   res.json({ message: "Welcome to application." });
 });
 
